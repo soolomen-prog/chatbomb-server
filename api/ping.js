a8b9f8463ec4a119a451705070591a0beeb76444
soolomen-prog <soolomen@gmail.com>
2025-11-12 12:03:53 +0100

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,3 @@
-// api/ping.js
-module.exports = (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json({ ok: true, pong: true, time: Date.now() });
-};
+export default async function handler(req, res) {
+  res.status(200).json({ ok: true });
+}
