700e90cf43b67f3e74374e1283fe66b0d99407b3
soolomen-prog <soolomen@gmail.com>
2025-11-20 22:16:00 +0100

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,3 @@
-export default async function handler(req, res) {
-  res.status(200).json({ ok: true });
+export default function handler(req, res) {
+  res.status(200).json({ ok: true, time: Date.now() });
 }
